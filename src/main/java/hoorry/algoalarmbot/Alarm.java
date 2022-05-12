905c48d107274716f44a5cd3078d1a33a3785385
hsjang <hsjang0405@gmail.com>
2022-05-12 17:07:09 +0900

    ✨  요일 확인 후 월, 목에만 동작하도록 변경

--- src/main/java/hoorry/algoalarmbot/Alarm.java
+++ src/main/java/hoorry/algoalarmbot/Alarm.java
@@ -2,12 +2,17 @@ package hoorry.algoalarmbot;
 
 import hoorry.algoalarmbot.alarm.Members;
 import hoorry.algoalarmbot.alarm.SlackService;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class Alarm {
 
 	public static void main(String[] args) {
-		SlackService slackService = new SlackService(new Members());
-		slackService.postSlackMessage();
+		DayOfWeek today = LocalDate.now().getDayOfWeek();
+		if (today == DayOfWeek.MONDAY || today == DayOfWeek.THURSDAY) {
+			SlackService slackService = new SlackService(new Members());
+			slackService.postSlackMessage();
+		}
 	}
 
 }
