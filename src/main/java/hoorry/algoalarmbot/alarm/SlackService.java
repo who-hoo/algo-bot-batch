845865cb6ca29dc71a9d7aef892cb7fedf7dc601
hsjang <hsjang0405@gmail.com>
2022-05-16 11:13:23 +0900

    ✨  메시지 레이아웃에 이미지 추가

--- src/main/java/hoorry/algoalarmbot/alarm/SlackService.java
+++ src/main/java/hoorry/algoalarmbot/alarm/SlackService.java
@@ -4,11 +4,15 @@ import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.model.block.element.ImageElement;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import static com.slack.api.model.block.composition.BlockCompositions.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import static com.slack.api.model.block.Blocks.*;
 
 @Service
 @Slf4j
@@ -25,9 +29,18 @@ public class SlackService {
 	public void postSlackMessage() {
 		try {
 			MethodsClient methods = Slack.getInstance().methods(token);
+			String alertMessage = createAlertMessage();
 			ChatPostMessageRequest request = ChatPostMessageRequest.builder()
 				.channel(channel)
-				.text(createAlertMessage())
+				.text(alertMessage)
+				.blocks(asBlocks(
+					section(section -> section.text(markdownText(alertMessage))
+							.accessory(ImageElement.builder()
+							.imageUrl("https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA0MjRfMjI2%2FMDAxNjUwNzgyNTY4NTQ1.yFg8zmQ_BCcF0d1kwqTwruNPjRuHRYn7yfuQvJOwBOIg.zZeOvkzVHlSiQAAMSY4uJpRj6dskBv0UB81bhWE7gBEg.JPEG.lbhcook%2FKakaoTalk_20220412_124754714_21.jpg&type=a340")
+							.altText("cute dog")
+							.build())
+					)
+				))
 				.build();
 
 			methods.chatPostMessage(request);
@@ -37,7 +50,7 @@ public class SlackService {
 		}
 	}
 
-	private String createAlertMessage() throws IOException {
+	private String createAlertMessage() {
 		return "<@" + members.getThisTurn() + "> [" + LocalDate.now()
 			.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
 			+ "] 문제 제출해주세요!!🚨🚨🚨";
